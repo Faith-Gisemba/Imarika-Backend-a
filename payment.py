a09c5e5bfd9aab5401b79640430a39ab0225b245
gracekazungu <kazungungrace@gmail.com>
2023-05-19 16:45:42 +0300

    Added payment.py file that cariies the customer payment details

--- payment.py
+++ payment.py
@@ -1,11 +1,28 @@
 class Payment:
     def __init__(self):
         self.ordering = []
-
     def make_payment(self, customer, total, balance):
-        if balance < total:
+
+        if total <= 0:
+            return "Invalid amount"
+        elif balance < 0:
+            return "Invalid balance"
+        elif balance < total:
             return "Insufficient balance"
         else:
             balance -= total
             self.ordering.append((customer, total))
             return "Transaction successful"
+
+
+
+payment = Payment('Susan',1000)
+result = payment.make_payment(500)
+print(result) 
+
+
+
+
+
+
+
