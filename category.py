4f22d3a494b1dcfc52ba590e29c0c5f0e470fcb4
student <simiyunangila>
2023-05-19 17:09:33 +0300

    created instances for the out put and added the else statements

--- category.py
+++ category.py
@@ -4,12 +4,23 @@ class Category:
         self.description = description
         self.products = []
     def add_product(self, product):
-        self.products.append(product)
-        return f"{name} was added into the category"
+        if product in self.products:
+            self.products.append(product)
+            return f"{name} was added into the category"
+        else:
+            return f"{name} Already exist"
     def remove_product(self, product):
         if product in self.products:
             self.products.remove(product)
             return f"{name} Is removed from the category"
+        else:
+            return f"{name}does not exist in this category"
     def update_description(self, new_description):
         self.description = new_description
         return f"{name} description was updated"
+        category=new Category(name)
+
+
+        category= Category("mangoes","ripe medium-size")
+        category.name("oranges")
+        category.description("green oranges")
